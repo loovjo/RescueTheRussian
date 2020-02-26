969f8138de1ad387e10bc82badfbfffad8ddcb1b
loovjo <jonathan.loov@gmail.com>
2020-02-26 17:44:49 +0100

    Make shovels effective againts walls

--- code/tile.py
+++ code/tile.py
@@ -7,6 +7,7 @@ from copy import deepcopy
 import pygame
 from texture_asset import TextureAsset
 from tile_texture import *
+from entity import Shovel
 
 class Tile(ABC):
     def __init__(self, tile_id, tile_texture):
@@ -76,11 +77,18 @@ class Fragile(Wall):
         vel_to_check = entity.velocity[1] if is_above_or_below else entity.velocity[0]
         momentum_to_check = vel_to_check * entity.mass
 
+        broke = False
         if abs(momentum_to_check) > BREAK_MOMENTUM_MIN:
             print("bonk")
             if random.random() < self.break_prec:
-                world.tiles[at] = FLOOR_COBBLE()
-                world.onBreakWall(at[0], at[1])
+                broke = True
+
+        if isinstance(entity, Shovel):
+            broke = random.random() ** 3 < self.break_prec
+
+        if broke:
+            world.tiles[at] = FLOOR_COBBLE()
+            world.onBreakWall(at[0], at[1])
         return True
 
 def FLOOR_WOOD():
